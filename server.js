3721a4f7674aebee82caea1de3326b8f8effd07b
shravandoijad <shravandoijad4@gmail.com>
2025-02-01 03:09:03 +0530

    backend

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const express = require('express');
 const cors = require('cors');
 const app  = express()
 const cookieParser = require('cookie-parser')
 const dbconnect = require("./db/db")
 const userRoute = require('./Routes/user-route')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-require('dotenv').config();
 
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -22,6 +24,25 @@ app.options("*", cors(corsOptions));
 const port =  process.env.PORT || 3000
 
 app.use("/api", userRoute)
+
+
+app.post('/payment/pay', async (req, res) => {
+  try {
+      
+      const { amount } = req.body;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount * 100, 
+          currency: 'usd',
+      });
+
+      
+      res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send("Error creating payment intent");
+  }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
